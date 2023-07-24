54ccf23db08bc929a05853ce478a03daf108cec1
Tahar Jaafer <124438260+Znullptr@users.noreply.github.com>
2023-07-24 17:31:22 +0100

    Computing Pages Ranks with Spark

--- psxhax_pages_rank.py
+++ psxhax_pages_rank.py
@@ -3,8 +3,6 @@ import networkx as nx
 
 # Load the psxhax pages data from CSV
 pages_info_df = pd.read_csv('psxhax_pages_info.csv')
-# drop pages with no links
-pages_info_df = pages_info_df.dropna()
 # Create a directed graph
 G = nx.DiGraph()
 
@@ -15,22 +13,25 @@ for index, row in pages_info_df.iterrows():
 print('Calculating PageRank values please wait ...')
 for index, row in pages_info_df.iterrows():
     src_page_id = row['page_id']
-    links = row['links'].split(',')
-    for link in links:
-        # Search for linked_page id
-        matching_row = pages_info_df.loc[pages_info_df['page_url'] == link]
-        if not matching_row.empty:
-            dest_page_id = matching_row.index[0]
-            G.add_edge(src_page_id, dest_page_id)
+    # Check if there are links in links column
+    links = row['links']
+    if pd.notna(links):
+        links = row['links'].split(',')
+        for link in links:
+            # Search for dest_page id
+            matching_row = pages_info_df.loc[pages_info_df['page_url'] == link]
+            if not matching_row.empty:
+                dest_page_id = matching_row.index[0]
+                G.add_edge(src_page_id, dest_page_id)
 
 # Run the PageRank algorithm
-pagerank_scores = nx.pagerank(G)
+pagerank_scores = nx.pagerank(G, max_iter=50, alpha=0.15)
 
 # Assign the PageRank scores to the psxhax pages dataframe
-pages_info_df['pagerank'] = [pagerank_scores[page_id] for page_id in pages_info_df['page_id']]
+pages_info_df['page_rank'] = [pagerank_scores[page_id] for page_id in pages_info_df['page_id']]
 
 # Sort the psxhax pages based on PageRank scores
-ranked_pages_df = pages_info_df.sort_values(by='pagerank', ascending=False)
+ranked_pages_df = pages_info_df.sort_values(by='page_rank', ascending=False)
 
 # Save the ranked psxhax pages information to CSV
 ranked_pages_df.to_csv('ranked_psxhax_pages.csv', index=False)
--- psxhax_pages_rank_spark.py
+++ psxhax_pages_rank_spark.py
@@ -8,22 +8,28 @@ spark = SparkSession.builder.appName("PageRank").getOrCreate()
 # Load the psxhax pages data from CSV as a DataFrame
 pages_info_df = spark.read.csv(sys.argv[1], header=True)
 
-# Drop pages with no links
-pages_info_df = pages_info_df.dropna()
-
 # Create vertices DataFrame
 vertices_df = pages_info_df.select("page_id").withColumnRenamed("page_id", "id")
 
-# Create links DataFrame
-urls_df = pages_info_df.select("page_id", "page_url")
-# Select here links and page_id as the src page_id from pages_info_df
-links_rows = pages_info_df.select("page_id", "links").rdd.flatMap(
-    lambda x: [(x[0], link.strip()) for link in x[1].split(",")]).collect()
-# Create an empty list for edges
+
+# Function to split links
+def split_links(row):
+    page_id, links = row
+    if links is not None:
+        links = [link.strip() for link in links.split(",")]
+    else:
+        links = []
+    return [(page_id, link) for link in links]
+
+
+# Select each link and its corresponding page_id from pages_info_df
+links_rows = pages_info_df.select("page_id", "links").rdd.flatMap(split_links).collect()
+
+# Create edges_list to store all the edges of the graph
 edges_list = []
 for row in links_rows:
     src_page_id, link = row
-    matching_row = urls_df.filter(urls_df.page_url == link).select("page_id").first()
+    matching_row = pages_info_df.filter(pages_info_df.page_url == link).select("page_id").first()
     if matching_row is not None:
         dest_page_id = matching_row.page_id
         edges_list.append((src_page_id, dest_page_id))
@@ -33,8 +39,9 @@ edges_df = spark.createDataFrame(edges_list, ["src", "dst"])
 
 # Create GraphFrame
 graph = GraphFrame(vertices_df, edges_df)
+
 # Run PageRank algorithm
-page_rank = graph.pageRank(maxIter=10)
+page_rank = graph.pageRank(maxIter=50, resetProbability=0.15)
 
 # Get the PageRank scores as a DataFrame
 page_rank_df = page_rank.vertices.select("id", "pagerank").withColumnRenamed("id", "page_id")
